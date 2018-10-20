d20608d768503d41992b9a8b480e25b2cafbc063
Xuri Li <dlshle@hotmail.com>
2018-10-19 21:09:19 -0500

    finished basic GUI, the code is messy though

--- wuziqi.js
+++ wuziqi.js
@@ -1,3 +1,116 @@
+var game;
+var canvas = document.createElement('canvas');
+var div = document.getElementById('wzq');
+canvas.width = 640;
+canvas.height = 640;
+canvas.addEventListener('click', function(event){
+	executeNext(event.offsetX,event.offsetY);
+},false);
+div.appendChild(canvas);
+ctx = canvas.getContext('2d');
+var pieceSize;
+var gameState = 0;
+
+function createNewWuziQi(){
+	createNewGame(19,5);
+}
+
+function createNewGame(diff, wc){
+	game = new gridGame(diff,diff,wc);
+	pieceSize = 640/diff;
+	newGame();
+	gameState = 0;
+}
+
+function playerDrawPiece(x,y){
+	var gridX, gridY;
+	gridX = parseInt(x/pieceSize);
+	gridY = parseInt(y/pieceSize);
+	var p = game.turn;
+	//-1 err, 0 normal, >0 win
+	switch(game.place(gridX,gridY)){
+		case 0:
+		//normal, next turn	
+			drawPiece(p,gridX,gridY);
+			return ;
+		case 1:
+		//p1 wins
+			drawPiece(p,gridX,gridY);
+			gameState = 2;
+			return ;
+		case 2:
+		//p2 wins
+			drawPiece(p,gridX,gridY);
+			gameState = 2;
+			return ;
+		default:
+		//err
+			return ;
+	}
+}
+
+function executeNext(x,y){
+	switch(gameState){
+		case 0:
+			//waiting for the first piece to be drawn
+			playerDrawPiece(x,y);
+			gameState = 1;
+			break;
+		case 1:
+			//on game
+			playerDrawPiece(x,y);
+			break;
+		case 2:
+			//got a winner(to case 0)
+			newGame();
+			break;
+		case 3:
+			//tba
+
+			break;
+		case 4:
+			//tba
+
+			break;
+		default:
+	}
+}
+
+function getMouseXY(event){
+	executeNext(event.offsetX,event.offsetY);
+}
+
+function drawGrid(){
+	for(var i=pieceSize;i<640;i+=pieceSize){
+		ctx.beginPath();
+		ctx.moveTo(0,i);
+		ctx.lineTo(640,i);
+		ctx.stroke();
+		ctx.moveTo(i,0);
+		ctx.lineTo(i,640);
+		ctx.stroke();
+	}
+	
+}
+
+function drawPiece(p,x,y){
+	var r = pieceSize/2;
+	var cx = x*pieceSize+r;
+	var cy = y*pieceSize+r;
+	ctx.beginPath();
+	ctx.arc(cx,cy,r,0,2*Math.PI,false);
+	if(p==1)
+		ctx.fill();
+	ctx.stroke();		
+}
+
+function newGame(){
+	game.newGame();
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawGrid();
+	gameState = 0;
+}
+
 function gridGame(w,h,winCount){
 	//0 nothing, 1 p1, 2 p2
 	this.grid = [];
@@ -26,18 +139,18 @@ function gridGame(w,h,winCount){
 	this.place = function(x,y){
 		if(this.win!=0){
 			console.log("Player "+this.win+" has won the game. Please start a new round.");
-			return ;
+			return -1;
 		}
 		//validate location
 		if(!this.isValidLocation(x,y)){
 			console.log("invalid location");
 			//try again
-			return ;
+			return -1;
 		}
 		if(this.grid[x][y]!=0){
 			console.log("already has a piece on "+x+","+y);
 			//try again
-			return ;
+			return -1;
 		}
 		//place on the grid
 		this.grid[x][y]=this.turn;
@@ -46,7 +159,7 @@ function gridGame(w,h,winCount){
 			//winning
 			this.win = this.turn;
 			console.log("Player "+this.turn+" wins");
-			return ;
+			return this.turn;
 		}
 		//take turn
 		if(this.turn==1)
@@ -55,6 +168,7 @@ function gridGame(w,h,winCount){
 			this.turn=1;
 
 		//wait for the next piece
+		return 0;
 	}
 
 	this.isValidLocation = function(x,y){
